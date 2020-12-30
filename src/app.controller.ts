f83d5e3c0da76c36d66d93296ddc10dd2a4265c0
문진석 <m_jinseok@naver.com>
2020-12-30 23:23:41 +0900

    GET /hello 컨트롤러 연습

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // GET /hello 시 sayHello() 함수를 실행
+  // expressjs에서처럼 라우터를 만들 필요 없음
+  @Get('/hello' /* url */)
+  sayHello(): string {
+    return 'Hello everyone';
+  }
 }
