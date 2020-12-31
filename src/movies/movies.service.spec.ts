c7f2c0a4d3af7ae2eecaec8a76d2d6fde3fbc554
문진석 <m_jinseok@naver.com>
2020-12-31 23:02:17 +0900

    유닛 테스트 기초

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -12,7 +12,14 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // 테스트하는 부분 (individual test)
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be 4', () => {
+    // 테스트하는 함수
+    expect(2 + 2).toEqual(4); // passed
+    // expect(2 + 2).toEqual(5); // failed
   });
 });
