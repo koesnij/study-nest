5a5c2c73e2e25ab0bd3535a09ba540de49fa37fc
문진석 <m_jinseok@naver.com>
2020-12-31 20:10:12 +0900

    컨트롤러 기능 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 /**
  * nest generate controller :
@@ -12,23 +21,31 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id' /* 이름이 같아야 함*/) // parameter를 가져오는 방법
+  @Get('search') // search가 get(:id)보다 밑에 있으면 NestJS는 search를 id로 판단
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':id' /* 이름이 같아야 함*/) // parameter를 가져오는 방법
   getOne(@Param('id' /* 이름이 같아야 함*/) movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData /* Body 가져오는 방법 */) {
+    return movieData; // return JSON data
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
