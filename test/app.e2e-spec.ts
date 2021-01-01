f8660024b9c5e5c4180f813d0fb0eca952510b47
문진석 <m_jinseok@naver.com>
2021-01-01 11:02:08 +0900

    DELETE /movies 404 e2e 테스팅

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -35,5 +35,8 @@ describe('AppController (e2e)', () => {
         .send({ title: 'Test Movie', genres: ['fortest'], year: 1970 })
         .expect(201);
     });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
